f9580a1363fe788ecbbcae0c5146e1bad3a2b841
Kamfeskaya <tatendachris@gmail.com>
2023-01-24 02:04:17 +0200

    refactor: remove unnecessary files

---
+++ src/components/icons/FilterIcon.tsx
@@ -0,0 +1,5 @@
+export default function FilterIcon() {
+  return(
+    <></>
+  )
+}
---
+++ src/components/icons/index.ts
@@ -0,0 +1,13 @@
+import FilterIcon from "@/components/icons/FilterIcon";
+import NotificationBellIcon from "@/components/icons/NotificationBellIcon";
+import RatingStarIcon from "@/components/icons/RatingStarIcon";
+import SearchIcon from "@/components/icons/SearchIcon";
+import ShoppingCartIcon from "@/components/icons/ShoppingCartIcon";
+
+export {
+  FilterIcon,
+  NotificationBellIcon,
+  RatingStarIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+}
